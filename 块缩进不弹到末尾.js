a7027315ce0bf5f0ff6bac3217eaccccf0f76a6c
Seagull <69051504+5eagull@users.noreply.github.com>
2021-05-15 02:34:44 +0800

    Delete 块缩进不弹到末尾.js

--- 块缩进不弹到末尾.js
+++
@@ -1,7 +0,0 @@
-plugin.userjs.register('outdentAtOrigin', {
-    before_nodeView_outdent(nv) {
-        nv.$node.nextAll('.node').each(function() {
-            this.nv.indent();
-        });
-    }
-});
